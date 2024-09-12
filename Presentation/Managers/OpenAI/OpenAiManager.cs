527339d996f6d59032ee3493bfc4c60142798b81
josedanielcr <jdcanales1905@gmail.com>
2024-09-12 17:10:13 -0600

    Fix: prompt file path

--- Presentation/Managers/OpenAI/OpenAiManager.cs
+++ Presentation/Managers/OpenAI/OpenAiManager.cs
@@ -7,6 +7,7 @@ namespace Gmail_To_YNAB_Transaction_Automation_API.Managers.OpenAI
     {
         private readonly IOpenAiService _openAiService;
         private readonly IConfiguration _configuration;
+        private readonly string _prompPath = Path.Combine(Directory.GetCurrentDirectory(), "Managers", "OpenAI", "prompt.txt");
 
         public OpenAiManager(IOpenAiService openAiService, IConfiguration configuration)
         {
@@ -15,10 +16,10 @@ namespace Gmail_To_YNAB_Transaction_Automation_API.Managers.OpenAI
         }
         public Task<string> GetParsedTransactionFromOpenAiChatGpt(string emailBody)
         {
-            if(emailBody == null || emailBody == "") throw new ArgumentNullException(emailBody);
-
-            string prompt = File.ReadAllText(_configuration["PromptFilePath"] ?? "");
-            if(prompt == "" || prompt == null)  throw new ArgumentNullException(prompt);
+            if(string.IsNullOrEmpty(emailBody)) throw new ArgumentNullException(emailBody);
+            
+            string prompt = File.ReadAllText(_prompPath);
+            if(string.IsNullOrEmpty(prompt))  throw new ArgumentNullException(prompt);
 
             // build the prompt
             string finalPrompt = prompt + emailBody;
--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd2c2803ab6cc3aec2d4a4cbeb16766a30c49b1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2405bb494d3aacc186a5542a01eb93657c36f465")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
