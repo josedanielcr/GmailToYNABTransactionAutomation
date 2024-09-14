ebef9b53c8a63911c6b99bbdc6e49c98681d57e5
josedanielcr <jdcanales1905@gmail.com>
2024-09-13 18:27:25 -0600

    Fix: file reading

--- Presentation/Managers/OpenAI/OpenAiManager.cs
+++ Presentation/Managers/OpenAI/OpenAiManager.cs
@@ -7,7 +7,7 @@ namespace Gmail_To_YNAB_Transaction_Automation_API.Managers.OpenAI
     {
         private readonly IOpenAiService _openAiService;
         private readonly IConfiguration _configuration;
-        private readonly string _prompPath = Path.Combine(Directory.GetCurrentDirectory(), "Managers", "OpenAI", "prompt.txt");
+        private readonly string _promptPath = Path.Combine(Directory.GetCurrentDirectory(), "Managers", "OpenAI", "prompt.txt");
 
         public OpenAiManager(IOpenAiService openAiService, IConfiguration configuration)
         {
@@ -18,7 +18,7 @@ namespace Gmail_To_YNAB_Transaction_Automation_API.Managers.OpenAI
         {
             if(string.IsNullOrEmpty(emailBody)) throw new ArgumentNullException(emailBody);
             
-            string prompt = File.ReadAllText(_prompPath);
+            string prompt = File.ReadAllText(_promptPath);
             if(string.IsNullOrEmpty(prompt))  throw new ArgumentNullException(prompt);
 
             // build the prompt
--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2405bb494d3aacc186a5542a01eb93657c36f465")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+527339d996f6d59032ee3493bfc4c60142798b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
